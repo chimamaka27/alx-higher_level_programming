6793d227b40db0294ba35c2b55ed4dc36f4a5e11
chimamka27 <chimamaka220@gmail.com>
2024-05-20 05:02:49 -0700

    added tasks

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
+print(f"{number}", end=" ")
 if number > 0:
-    print("Number:", number, "is positive")
-elif number == 0:
-    print("Number:", number, "is zero")
+    print("is positive")
+elif number < 0:
+    print("is negative")
 else:
-    print(f"{Number} is negative")
+    print("is zero")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number <0:
-        last-digit=number %-(10)
-        print(-(last-digit), end=")
-        else:
-        last-digit = number % 10
-        print(last-digit, end=")
-        return abs(last_digit)
+    last_digit = number % 10 if number >= 0 else -number % 10
+    print(f"{last_digit}", end="")
+    return last_digit
